ceface0e37804380e5c4e9bc361e2ae674d6a18b
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-01 19:38:31 -0300

    :tada: init: first commit with initial project structure

---
+++ api-desafio-encurtarUrl/src/main/java/com/ggomes/api_desafio/ApiDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioApplication.class, args);
+	}
+
+}
---
+++ api-desafio-encurtarUrl/src/test/java/com/ggomes/api_desafio/ApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
